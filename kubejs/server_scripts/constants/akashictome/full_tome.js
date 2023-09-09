a835be9f408c21555a727c3886eb8a550c09c480
KuryKat <57152799+KuryKat@users.noreply.github.com>
2023-09-09 17:31:01 -0400

    Update Akashic Tome to include more books!
    Old players will need to craft a new Tome to receive the new books!

--- kubejs/server_scripts/constants/akashictome/full_tome.js
+++ kubejs/server_scripts/constants/akashictome/full_tome.js
@@ -1,5 +1,12 @@
 const fullAkashicTome = Item.of('akashictome:tome', {
     'akashictome:data': {
+        alchemistry: {
+            Count: 1,
+            id: 'patchouli:guide_book',
+            tag: {
+                'patchouli:book': 'alchemistry:alchemistry_book'
+            }
+        },
         apotheosis: {
             Count: 1,
             id: 'patchouli:guide_book',
@@ -31,6 +38,20 @@ const fullAkashicTome = Item.of('akashictome:tome', {
             Count: 1,
             id: 'immersiveengineering:manual'
         },
+        industrialforegoing: {
+            Count: 1,
+            id: 'patchouli:guide_book',
+            tag: {
+                'patchouli:book': 'industrialforegoing:industrial_foregoing'
+            }
+        },
+        littlelogistics: {
+            Count: 1,
+            id: 'patchouli:guide_book',
+            tag: {
+                'patchouli:book': 'littlelogistics:guide'
+            }
+        },
         occultism: {
             Count: 1,
             id: 'occultism:dictionary_of_spirits'
@@ -43,9 +64,34 @@ const fullAkashicTome = Item.of('akashictome:tome', {
             Count: 1,
             id: 'rftoolsbase:manual'
         },
+        sebastrnlib: {
+            Count: 1,
+            id: 'patchouli:guide_book',
+            tag: {
+                'patchouli:book': 'sebastrnlib:sebastrn_mods_guide_book'
+            }
+        },
         securitycraft: {
             Count: 1,
             id: 'securitycraft:sc_manual'
+        },
+        silentgear: {
+            Count: 1,
+            id: 'silentgear:guide_book'
+        },
+        tetra: {
+            Count: 1,
+            id: 'tetra:holo',
+            tag: {
+                "holo/core": "holo/core",
+                "holo/core_material": "frame/dim",
+                "holo/frame": "holo/frame",
+                "holo/frame_material": "core/ancient",
+                "holo/repo": "holo/repo",
+                "holo/repo_material": "repo/default",
+                "holo/scanner": "holo/scanner",
+                "holo/scanner_material": "scanner/default"
+            }
         }
     }
 });
