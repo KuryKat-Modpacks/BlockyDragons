4ea99ab3f45d53e93b8a1b06f752da612a306f24
KuryKat <57152799+KuryKat@users.noreply.github.com>
2023-09-10 11:27:46 -0400

    Prevent SilentGems Ores from spawning

--- kubejs/server_scripts/base/worldgen/disable_ores.js
+++ kubejs/server_scripts/base/worldgen/disable_ores.js
@@ -1,4 +1,7 @@
 ServerEvents.highPriorityData((event) => {
+    
+    // Disable Occultism Ores
+    
     event.addJson('occultism:forge/biome_modifier/add_silver_ore', {
         type: 'forge:remove_features',
         features: 'occultism:silver_ore',
@@ -9,6 +12,18 @@ ServerEvents.highPriorityData((event) => {
     event.addJson('occultism:forge/biome_modifier/add_deepslate_silver_ore', {
         type: 'forge:remove_features',
         features: 'occultism:silver_ore_deepslate',
+        step: 'underground_ores',
+        biomes: '#minecraft:is_overworld'
+    });
+
+    
+    // Disable Silent Gems Ores
+    
+    event.addJson('silentgems:forge/biome_modifier/overworld_ores', {
+        type: 'forge:remove_features',
+        features: [
+            'silentgems:silver_ore',
+        ],
         step: 'underground_ores',
         biomes: '#minecraft:is_overworld'
     });
