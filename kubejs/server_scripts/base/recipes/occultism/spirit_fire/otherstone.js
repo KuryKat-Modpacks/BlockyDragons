9689685ac52348dc746f261b76bc8a2f844d9f95
KuryKat <57152799+KuryKat@users.noreply.github.com>
2023-09-16 05:53:07 -0400

    Allow any kind of stone to become otherstone

---
+++ kubejs/server_scripts/base/recipes/occultism/spirit_fire/otherstone.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    event.replaceInput(
+        { id: "occultism:spirit_fire/otherstone" }, 
+        "minecraft:andesite", 
+        { tag: "forge:stone" }
+    )
+})
