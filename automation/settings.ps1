9dd8c3943816c59db8f8b337ef80d11c73e1aaab
KuryKat <57152799+KuryKat@users.noreply.github.com>
2023-10-04 11:52:07 -0400

    Preparing to launch 🚀

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -29,11 +29,11 @@ $MODPACK_NAME = "blocky-dragons"
 $CLIENT_NAME = "BlockyDragons"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "v1.6.3"
+$MODPACK_VERSION = "v1.6.4"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "v1.6.2"
+$LAST_MODPACK_VERSION = "v1.6.3"
 
 # =====================================================================//
 #  CHANGELOG SETTINGS
